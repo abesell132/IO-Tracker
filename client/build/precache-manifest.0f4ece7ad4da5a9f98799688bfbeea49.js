23ffc0b9d0c24197119190d4d191a65a081d76b2
Abe <abesell132@gmail.com>
2019-05-01 22:44:08 -0400

    allow production

---
+++ client/build/precache-manifest.0f4ece7ad4da5a9f98799688bfbeea49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cb0765147cf638b7a42e",
+    "url": "/static/js/main.cb076514.chunk.js"
+  },
+  {
+    "revision": "fca1d712355e1afed0f5",
+    "url": "/static/js/1.fca1d712.chunk.js"
+  },
+  {
+    "revision": "cb0765147cf638b7a42e",
+    "url": "/static/css/main.392d1a09.chunk.css"
+  },
+  {
+    "revision": "11d4093ec62ad331c63665874681fd21",
+    "url": "/index.html"
+  }
+];
